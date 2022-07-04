b94684490b5e99751c75d94842e8239005b009f1
hellodajana <dajanasmolyakov@gmail.com>
2022-07-04 14:39:08 +0200

    made loadAdvice function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import { useState, useEffect } from "react";
 import diceButton from "./images/icon-dice.svg";
 import dividerMobile from "./images/pattern-divider-mobile.svg";
 import dividerDesktop from "./images/pattern-divider-desktop.svg";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+  const loadAdvice = async () => {
+    const API_LINK = "https://api.adviceslip.com/advice";
+    const res = await fetch(API_LINK, { cache: "no-cache" });
+    const data = await res.json();
+    const show = data.slip;
+
+    setAdvice(show);
+  };
+
+  useEffect(() => {
+    loadAdvice();
+  }, []);
+
   return (
     <div className="container">
       <h1 id="advice" className="container__advice">
-        Advice #
+        Advice #{advice.id}
       </h1>
-      <p id="text" className="container__text"></p>
+      <p id="text" className="container__text">
+        {advice.advice}
+      </p>
       <picture>
         <source media="(min-width: 768px)" srcSet={dividerDesktop} />
-        <img src={dividerMobile} alt="divider" class="container__divider" />
+        <img src={dividerMobile} alt="divider" className="container__divider" />
       </picture>
-      <div class="container__btn">
-        <button class="container__btn-dice" id="btn">
+      <div className="container__btn">
+        <button onClick={loadAdvice} className="container__btn-dice" id="btn">
           <img src={diceButton} alt="dice" />
         </button>
       </div>
